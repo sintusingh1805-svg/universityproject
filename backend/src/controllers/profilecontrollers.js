74d5df973b3e040f9b3c71f1be3d384879ab58d9
premverma07 <prembe357@gmail.com>
2026-07-02 18:36:43 +0530

    folder create routes and middlewares

---
+++ backend/src/controllers/profilecontrollers.js
@@ -0,0 +1,15 @@
+import { User } from "../models/user.model.js";
+
+export const profile=async(req,res)=>{
+    try{
+        res.status(200).json({
+            success:true,
+            
+        })
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"internal error",
+        })
+    }
+}
---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,4 @@
+import express from "express";
+import  Router  from "express";
+
+export const router=
