c7e7b6aeebd2603cf53f1226be3fe4b3e3f970e7
Ritik Singh <sintusingh1805@gmail.com>
2026-07-03 11:50:53 +0530

    all files are fix and run server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,6 @@
 import express from "express";
-
+import {router} from  "./routes/user.routes.js";
 export const app=express();
+
+app.use(express.json());
+app.use("/api",router);
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import  Router  from "express";
-import registercontrollers from "../src/controllers/registercontrollers.js";
-import logincontrollers from "../src/controllers/logincontrollers.js";
-import profilecontrollers from "../src/controllers/profilecontrollers.js";
+import {register} from "../controllers/registercontrollers.js";
+import {login} from "../controllers/logincontrollers.js";
+import {profile} from "../controllers/profilecontrollers.js";
 
 export const router=express.Router();
 
 
-router.post("/register",registercontrollers);
-router.post("/login",logincontrollers);
-router.post("/profile",profilecontrollers);
+router.post("/register",register);
+router.post("/login",login);
+router.get("/profile",profile);
