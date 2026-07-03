c04af18ad71e8a7b4b2abdcc0ab1c570937175b4
Ritik Singh <sintusingh1805@gmail.com>
2026-07-03 10:59:31 +0530

    create routes files

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,4 +1,12 @@
 import express from "express";
 import  Router  from "express";
+import registercontrollers from "../src/controllers/registercontrollers.js";
+import logincontrollers from "../src/controllers/logincontrollers.js";
+import profilecontrollers from "../src/controllers/profilecontrollers.js";
 
-export const router=
+export const router=express.Router();
+
+
+router.post("/register",registercontrollers);
+router.post("/login",logincontrollers);
+router.post("/profile",profilecontrollers);
