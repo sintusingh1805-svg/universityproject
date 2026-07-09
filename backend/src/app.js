516f679672456224500e6e2bdff3abc45b0e2a48
premverma07 <prembe357@gmail.com>
2026-07-09 17:54:23 +0530

    app.js are modify

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {router} from  "./routes/user.routes.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
+import { router } from "./routes/student.routes.js";
 export const app=express();
 
 app.use(express.json());
 
 app.use("/api", router)
+app.use("/api/students",router)
 
 app.use(errorMiddleware);
